93f5b646d0a3481fc1c252f3a2e03f1efcb9db43
Kristof De Winter <s106749@ap.be>
2020-10-12 11:25:18 +0200

    init Rocket Assignment

---
+++ Assets/Scripts/FollowRocket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRocket : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(target.position.y, 0f, 300f), transform.position.z);
+    }
+}
---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int levelToLoad;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
